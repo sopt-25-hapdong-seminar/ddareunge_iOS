739525b8e3e093fa33c2728444a12f430b3d9ac8
Dongmin.yoon <ehdals8965@naver.com>
2019-11-23 15:38:46 +0900

    [Update] Implement Login View

---
+++ ddareunge_iOS/ddareunge_iOS/Sources/ViewControllers/LoginViewController.swift
@@ -0,0 +1,28 @@
+//
+//  LoginViewController.swift
+//  ddareunge_iOS
+//
+//  Created by 윤동민 on 2019/11/23.
+//  Copyright © 2019 SwiftHelloWorld. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    @IBOutlet weak var idTextField: UITextField!
+    @IBOutlet weak var pwTextField: UITextField!
+    @IBOutlet weak var signupButton: UIButton!
+    @IBOutlet weak var signinButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func doSignup(_ sender: Any) {
+    }
+    
+    @IBAction func doSignin(_ sender: Any) {
+    }
+}
